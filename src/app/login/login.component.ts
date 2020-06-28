ad853e26d35e37e9707eea8e55137890e1d5ab16
Manuel Florez <manuelflorezw@gmail.com>
2020-06-28 16:53:24 -0500

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { UsersService } from "app/users/users.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  //styleUrls: ["./login.component.css"]
+})
+export class LoginComponent {
+  email: string;
+  password: string;
+
+  constructor(public userService: UsersService,
+    public router: Router) { }
+
+  login() {
+    const user = { email: this.email, password: this.password };
+    this.userService.login(user).subscribe(data => {
+      this.userService.setToken(data.token);
+      this.router.navigate(['/dashboard']);
+    });
+  }
+}
